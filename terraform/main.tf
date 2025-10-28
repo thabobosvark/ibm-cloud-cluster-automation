2a6fa3d1d6bbe74ee1ff5a62d5d93aa3d1a9d30e
thabobosvark <thabobosvark@icloud.com>
2025-10-28 14:40:28 +0000

    feat: Complete Week 4 with working CI/CD pipeline
    - Fix GitHub Actions workflow file location and content
    - Deploy com3 node instead of com2 to avoid naming conflict
    - Update Terraform configuration for com3 deployment
    - Complete CI/CD pipeline with Terraform and Ansible integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,83 @@
-# Get existing VPC data
-data "ibm_is_vpc" "existing_vpc" {
-  name = var.vpc_name
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.84.3"
+    }
+  }
 }
 
-# Get existing subnet data
-data "ibm_is_subnet" "existing_subnet" {
-  name = var.subnet_name
+provider "ibm" {
+  region = "us-south"
 }
 
-# Get SSH key data
-data "ibm_is_ssh_key" "existing_key" {
-  name = var.ssh_key_name
+resource "ibm_is_vpc" "cluster_vpc" {
+  name = "hpc-cluster-vpc"
 }
 
-# Get image data for CentOS Stream 9
-data "ibm_is_image" "centos_stream_9" {
-  name = "ibm-centos-stream-9-amd64-12"
+resource "ibm_is_subnet" "cluster_subnet" {
+  name                     = "hpc-cluster-subnet"
+  vpc                      = ibm_is_vpc.cluster_vpc.id
+  zone                     = "us-south-1"
+  total_ipv4_address_count = 256
 }
 
-# Create 3rd compute node (com2)
-resource "ibm_is_instance" "com2_node" {
-  name    = "com2"
-  vpc     = data.ibm_is_vpc.existing_vpc.id
-  zone    = "eu-gb-2"
-  keys    = [data.ibm_is_ssh_key.existing_key.id]
-  image   = data.ibm_is_image.centos_stream_9.id
-  profile = "bx2-2x8"
+resource "ibm_is_security_group" "cluster_sg" {
+  name = "hpc-cluster-sg"
+  vpc  = ibm_is_vpc.cluster_vpc.id
+}
+
+resource "ibm_is_security_group_rule" "ssh" {
+  group     = ibm_is_security_group.cluster_sg.id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_security_group_rule" "internal" {
+  group     = ibm_is_security_group.cluster_sg.id
+  direction = "inbound"
+  remote    = "10.0.0.0/8"
+}
+
+resource "ibm_is_instance" "com3_node" {
+  name    = "com3-${formatdate("YYYYMMDD-hhmmss", timestamp())}"
+  image   = var.image_id
+  profile = var.profile
+  keys    = [var.ssh_key_id]
 
   primary_network_interface {
-    subnet = data.ibm_is_subnet.existing_subnet.id
+    subnet          = ibm_is_subnet.cluster_subnet.id
+    security_groups = [ibm_is_security_group.cluster_sg.id]
   }
 
-  tags = ["week4-assignment", "compute-node", "automated-deployment"]
+  vpc       = ibm_is_vpc.cluster_vpc.id
+  zone      = "us-south-1"
+  user_data = file("${path.module}/user-data.sh")
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
 }
 
-# Output important information
-output "com2_instance_id" {
-  value = ibm_is_instance.com2_node.id
+resource "ibm_is_floating_ip" "com3_fip" {
+  name   = "com3-floating-ip"
+  target = ibm_is_instance.com3_node.primary_network_interface[0].id
 }
 
-output "com2_private_ip" {
-  value = ibm_is_instance.com2_node.primary_network_interface[0].primary_ip[0].address
+output "com3_instance_id" {
+  value = ibm_is_instance.com3_node.id
 }
 
-output "com2_ssh_command" {
-  value = "ssh root@${ibm_is_instance.com2_node.primary_network_interface[0].primary_ip[0].address}"
+output "com3_private_ip" {
+  value = ibm_is_instance.com3_node.primary_network_interface[0].primary_ipv4_address
 }
 
-output "com2_status" {
-  value = ibm_is_instance.com2_node.status
+output "com3_public_ip" {
+  value = ibm_is_floating_ip.com3_fip.address
 }
